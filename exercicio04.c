4b07a248caf68413e64d94e925a137c756667024
Gabriela Rabelo <101715908+GabrielaNR@users.noreply.github.com>
2023-10-12 10:55:38 -0300

    Update and rename exercicio4.c to exercicio04.c

--- exercicio04.c
+++ exercicio04.c
@@ -1,4 +1,4 @@
-//Implemente um algoritmo que verifique se um vetor de inteiros VI, com N inteiros, est· em ordem crescente ou n„o.
+//Implemente um algoritmo que verifique se um vetor de inteiros VI, com N inteiros, est√° em ordem crescente ou n√£o.
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
@@ -6,31 +6,31 @@
 int main (void){
     setlocale(LC_ALL, "Portuguese");
 
-//declarando o tamanho do vetor e pedindo o dado ao usu·rio
+//declarando o tamanho do vetor e pedindo o dado ao usu√°rio
     int n;
     printf("Digite quantos itens possui o vetor: \n");
     scanf("%d", &n);
 
-//solicitando os dados do vetor ao usu·rio
+//solicitando os dados do vetor ao usu√°rio
     int vetor[n];
     printf("Digite os itens do vetor: \n");
     for (int i = 0; i < n; i++) {
         scanf("%d", &vetor[i]);
     }
-//checando se est· em ordem crescente
+//checando se est√° em ordem crescente
     int crescente = 1;
 
     for (int i = 0; i < n - 1; i++) {
         if (vetor[i] > vetor[i + 1]) {
             crescente = 0;
-            break;    //se encontrar um i maior o programa para a verificaÁ„o, pois j· sabemos que o vetor n„o est· em ordem crescente
+            break;    //se encontrar um i maior o programa para a verifica√ß√£o, pois j√° sabemos que o vetor n√£o est√° em ordem crescente
         }
     }
 
     if (crescente) {
-        printf("O vetor est· em ordem crescente!\n");
+        printf("O vetor est√° em ordem crescente!\n");
     } else {
-        printf("O vetor n„o est· em ordem crescente!\n");
+        printf("O vetor n√£o est√° em ordem crescente!\n");
     }
 
     return 0;
